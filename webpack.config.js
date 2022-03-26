b4e3a452a27c2ce926657457cbbfd916f8596a26
shinheewon01 <shw20231@gmail.com>
2022-03-27 02:03:34 +0900

    create project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+//import
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+//export
+module.exports={
+  //parcel index.html
+  //파일을 읽어들이기 시작하는 진입점 설정
+  entry: './js/main.js',
+  //결과물을 반환하는 설정
+  output: {
+    //  path: path.resolve(__dirname,'dist'),
+    //  filename: 'main.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          // 순서 중요!
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // 제외할 경로
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    })
+  ]
+} 
